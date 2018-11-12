32c0774b2112c3d3efeee2ab914d6ac4e38579e9
royoulion <royou@catears.org>
2018-11-12 03:35:41 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Character> vowels = new ArrayList<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+        vowels.add('y');
+
+        while(sc.hasNext()){
+            String line = sc.nextLine();
+
+            if(vowels.contains(line)){
+                
+            }
+        }
+
+
+
+
+    }
+}
